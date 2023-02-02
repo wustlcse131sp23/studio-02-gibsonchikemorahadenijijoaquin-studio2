b695049be8b23b93dc0f70f4d7ebb41de33088f9
jaden <jaden@172.27.90.127>
2023-02-02 14:11:02 -0600

    Hola Mamacita

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -1,29 +1,50 @@
 package studio2;
 
+import java.util.Scanner;
+
 public class Ruin {
 
 	public static void main(String[] args) 
 	{
+		Scanner in = new Scanner(System.in);
+	
+		System.out.println("How many days would you like to simulate? ");
+		int days = in.nextInt();
 		
-		int startAmount = 500;
-		int winLimit = 1000;
-		boolean winChance = Math.random()>= 0.5;
+		
+		
+		
+		int winLimit = 10;
+		
+		 for (int i = 1; i <= days; i ++)
+		 {
+			 int startAmount = 5;
+			 int loopCount = 0; 
+			 
 		while (startAmount != 0 && startAmount < winLimit)
 		{
+			boolean winChance = Math.random()>= .5;
+			
 			if (winChance == true)
 			{
 				startAmount ++;
 			}
 			else
 				startAmount --;
+		loopCount ++;
 		}
+		
 		if (startAmount == 0)
 		{
-			System.out.print("Ruin!");
+			System.out.println(loopCount + ": Ruin! Day: " + i);
 		}
 		else if (startAmount >= winLimit)
-		System.out.print("Success!");
+		{
+			System.out.println(loopCount + ": Success! Day: " + i);
+		}
+		
 
+		 }
 	}
-
 }
+
